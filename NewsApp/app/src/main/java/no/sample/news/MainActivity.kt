d3702397ab2ff6c652ef532471308e145d3a0923
Imran Baig <imranbaig@hotmail.co.uk>
2020-03-13 12:56:32 +0100

    simplified the news items.

--- NewsApp/app/src/main/java/no/sample/news/MainActivity.kt
+++ NewsApp/app/src/main/java/no/sample/news/MainActivity.kt
@@ -4,13 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 import no.sample.news.adaptor.NewsFeedAdapter
 import no.sample.news.api.DummyNews
-import no.sample.news.datatype.NewsItem
+import no.sample.news.gsontypes.NewsStory
 
 class MainActivity : AppCompatActivity(), View.OnClickListener /*Implementing OnClickListener*/ {
 
@@ -37,7 +35,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener /*Implementing On
     }
 
     // Refreshing the news feed
-    private fun updateNews( feed: ArrayList<NewsItem>) {
+    private fun updateNews( feed: ArrayList<NewsStory>) {
         adapter.list = feed // setting new feed ArrayList
         adapter.notifyDataSetChanged() // This notifies the adapter that data has changed. RecyclerView needs refresh
     }
@@ -45,7 +43,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener /*Implementing On
 
     override fun onClick(v: View?) {
 
-        var newsItem = v?.tag as NewsItem
+        var newsItem = v?.tag as NewsStory
 
         Intent(this, WebViewActivity::class.java).also {intent->
             intent.putExtra(WebViewActivity.LINK,newsItem.url)
