b54d364ea67a3de1c16dbb4c48b0e657ae24072f
Imran Baig <imranbaig@hotmail.co.uk>
2020-03-15 09:03:30 +0100

    commented the code

--- NewsApp/app/src/main/java/no/sample/news/api/vg/VgFeedClient.kt
+++ NewsApp/app/src/main/java/no/sample/news/api/vg/VgFeedClient.kt
@@ -10,43 +10,44 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class VgFeedClient {
 
-    private lateinit var service :  VgService
-
+    private lateinit var service :  VgService // This is service interface
 
     constructor()
     {
         var retrofit = Retrofit.Builder()
-            .baseUrl("https://www.vg.no")
-            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("https://www.vg.no") // Base URL for the endpoint
+            .addConverterFactory(GsonConverterFactory.create()) // Using GSON Converter which will automatically handles parsing
             .build()
 
         service = retrofit.create(VgService::class.java)
 
     }
 
-    fun getNewsFeed(newsListener: NewsListener){
+    fun getNewsFeed( newsListener: NewsListener )
+    {
 
-        newsListener.showProgress(true)
-        var call = service.getNewsFeed()
+        newsListener.showProgress(true) // showing progress via callback
 
-        call.enqueue(object :Callback<ArrayList<NewsStory>>
+        var call = service.getNewsFeed("json", 10) // supplying the query parameters in the call
+
+        call.enqueue(object :Callback<ArrayList<NewsStory>>  // Calling the endpoint asynchronously
         {
-            override fun onFailure(call: Call<ArrayList<NewsStory>>, t: Throwable) {
-                newsListener.showProgress(false)
+            override fun onFailure(call: Call<ArrayList<NewsStory>>, t: Throwable) { // Handling failure
+                newsListener.showProgress(false) // ending progress. onFailure is called in primary thread
 
-                newsListener.onNewsError()
+                newsListener.onNewsError() // UI will show error message
             }
 
             override fun onResponse( call: Call<ArrayList<NewsStory>>, response: Response<ArrayList<NewsStory>>) {
-                newsListener.showProgress(false)
+                newsListener.showProgress(false) // ending progress. onResponse is called in primary thread
 
-               if(response.isSuccessful)
+               if(response.isSuccessful) // if the response is successful
                {
-                   newsListener.onNewsSuccess(response.body())
+                   newsListener.onNewsSuccess(response.body()) // give back the ArrayList<NewsStory> to UI via
                }
                else
                {
-                   newsListener.onNewsError()
+                   newsListener.onNewsError() // UI will show error message
                }
             }
         })
--- NewsApp/app/src/main/java/no/sample/news/api/vg/VgService.kt
+++ NewsApp/app/src/main/java/no/sample/news/api/vg/VgService.kt
@@ -3,9 +3,12 @@ package no.sample.news.api.vg
 import no.sample.news.gsontypes.NewsStory
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface VgService {
 
-    @GET("/rss/feed/?format=json")
-    fun getNewsFeed(): Call<ArrayList<NewsStory>>
+    @GET("/rss/feed/") // Using annotation for the endpoint
+    fun getNewsFeed(@Query("format") format: String, @Query("limit") limit: Int ): Call<ArrayList<NewsStory>>
+
+
 }
